54e7e6c2dddc90f88429443acdf34e4ba4e4530c
Xett <3954937+Xett@users.noreply.github.com>
2019-04-22 04:37:20 +1000

    remove update submodules

--- scripts/update_submodules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-update_submodules()
-{
-   git submodule init
-   git submodule update
-   git submodule foreach '[ "$path" == "Wordpress-Database-Template" ] || git pull origin master'
-}
--- update_submodules.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Import variables
-SCRIPT=$(readlink -f "$0")
-PROJECT_DIRECTORY=$(dirname "$SCRIPT")
-. $PROJECT_DIRECTORY"/scripts/update_submodules.sh"
-update_submodules
